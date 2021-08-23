e53e3cc7ece0084012d9667fed5b28ded1ad468c
Aimee Boyle <aimee.boyle@kainos.com>
2021-08-23 13:22:06 +0100

    add project structure

---
+++ src/main/java/com/kainos/jobnight/JobnightApplication.java
@@ -0,0 +1,13 @@
+package com.kainos.jobnight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobnightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobnightApplication.class, args);
+	}
+
+}
