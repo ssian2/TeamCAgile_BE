2ee9366095860f08eb73d4ef52fd7321ea559b2c
ymtyroni <lukasz.calka@protonmail.com>
2021-09-02 09:34:23 +0200

    fix springdoc

--- src/test/java/com/kainos/jobnight/JobRoleAPITest.java
+++ src/test/java/com/kainos/jobnight/JobRoleAPITest.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
-import springfox.documentation.oas.mappers.StyleEnumMapper;
+//import springfox.documentation.oas.mappers.StyleEnumMapper;
 
 import static com.kainos.jobnight.Util.loadResourceAsString;
 import static org.junit.jupiter.api.Assertions.fail;
